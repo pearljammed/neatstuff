9bb4a5b87ce8d240ea5f9ceefdd9f7f6a67b1ce2
Edward Severson <dkane75@gmail.com>
2015-10-18 19:28:03 -0400

    Create dont_be_retarded.sh

---
+++ dont_be_retarded.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "Man oh man this is a cool program, I mean..........";
+echo "..... if you don't think so, U R retarded, but......."
+echo ".........DON'T BE RETARDED, HAHAHAHAHHAHAHHAHA, :)";
+echo "(man, whew, another good program!)"
